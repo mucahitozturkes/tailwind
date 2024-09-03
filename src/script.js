876cfd823f83b0be8ae87399a0a42ff5ba8eaf62
SaGe <m.ozturkes@gmail.com>
2024-09-03 12:56:23 +0300

    updated

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const hideButton = document.querySelector("#open-menu")
+const sideMenu = document.querySelector("#sidebar")
+
+hideButton.addEventListener("click", function(){
+    sideMenu.classList.toggle("dn")
+});
